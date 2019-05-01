bdab003f94aa2b24046c8d02cb7db27b37f0c6dc
Suresh Rathore <sureshrathore309@gmail.com>
2019-05-01 12:30:43 +0530

    commited

---
+++ src/com/java/interview/ReverseAString.java
@@ -0,0 +1,21 @@
+package com.java.interview;
+/**
+ * 
+ * @author suresh-rathore
+ *
+ */
+public class ReverseAString {
+
+	public static String reverseString(String s) {
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			result.append(s.charAt(s.length() - i - 1));
+		}
+		return result.toString();
+	}
+
+	public static void main(String[] args) {
+		String test = "To be reversed!";
+		System.out.println(reverseString(test));
+	}
+}
---
+++ src/com/java/interview/ReverseWordsinString.java
@@ -0,0 +1,35 @@
+package com.java.interview;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author suresh-rathore
+ *
+ */
+
+public class ReverseWordsinString {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		in.close();
+
+		System.out.println("Original Array: " + s);
+		System.out.println("Reversed Array: " + reverseLine(s));
+	}
+
+	private static String reverseLine(String s) {
+		if (s == null || s.trim().isEmpty())
+			return s;
+
+		String[] arr = s.split(" ");
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = arr.length - 1; i > -1; i--) {
+			sb.append(arr[i]).append(" ");
+		}
+
+		return sb.toString().trim();
+
+	}
+}
