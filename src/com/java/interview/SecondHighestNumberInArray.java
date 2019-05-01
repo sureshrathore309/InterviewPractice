614ae18bb217cb02d47f4ba1b2d763e59c0c1f42
Suresh Rathore <sureshrathore309@gmail.com>
2019-05-01 11:31:38 +0530

    second largest program

---
+++ src/com/java/interview/SecondHighestNumberInArray.java
@@ -0,0 +1,24 @@
+package com.java.interview;
+
+public class SecondHighestNumberInArray {
+	public static void main (String []args) {
+		int arr[] = {14,46,47,94,52,86,36,94,89};
+		int largest =arr[0];
+		int secondLargest=arr[0];
+		System.out.println("The Given Array is : ");
+		for (int i=0;i <arr.length;i++) {
+			System.out.print(arr[i]+"\t");
+		}
+		
+		for (int i=0;i<arr.length;i++) {
+			if (arr[i]>largest) {
+				secondLargest=largest;
+				largest=arr[i];
+			}
+			else if (arr[i]>secondLargest && arr[i] !=largest) {
+				secondLargest=arr[i];
+			}
+		}
+		System.out.println("\n Second Largest number is : "+secondLargest);
+	}
+}
