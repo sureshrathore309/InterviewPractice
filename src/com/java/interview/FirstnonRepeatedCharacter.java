94601c9952d68ee584179930253083a7e7650631
Suresh Rathore <sureshrathore309@gmail.com>
2019-05-01 11:19:02 +0530

    initial commit

---
+++ src/com/java/interview/FirstnonRepeatedCharacter.java
@@ -0,0 +1,47 @@
+package com.java.interview;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author suresh-rathore
+ *
+ */
+public class FirstnonRepeatedCharacter {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		in.close();
+
+		System.out.println("First non repeated character is " + firstNonRepeatedCharacterInAString(s));
+	}
+
+	public static char firstNonRepeated(String s) {
+		int[] letters = new int[26];
+		for (int i = 0; i < s.length(); i++) {
+			letters[s.charAt(i) - 'a']++;
+		}
+
+		for (int i = 0; i < s.length(); i++) {
+			if (letters[s.charAt(i) - 'a'] == 1) {
+				return (char) (i + 'a');
+			}
+		}
+
+		return 0;
+	}
+	
+	public static char firstNonRepeatedCharacterInAString(String s) {
+		char[] characters = s.toCharArray();
+		int j;
+		for (int i = 0; i < characters.length; i++) {
+			for (j = 0; j < characters.length; j++) {
+				if (i != j && characters[i] == characters[j])
+					break;
+			}
+			if (j == characters.length)
+				return characters[i];
+		}
+		return '0';
+	}
+}
---
+++ src/com/java/interview/PrintingPatterns.java
@@ -0,0 +1,24 @@
+package com.java.interview;
+
+/**
+ * 
+ * @author suresh-rathore
+ *
+ */
+/*
+ * How to Print a Pattern in Java. ex. Pyramid Pattern
+ */
+public class PrintingPatterns {
+	public static void main(String[] args) {
+		drawPyrmidPattern(5, "* ");
+	}
+
+	public static void drawPyrmidPattern(int n, String block) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print(block);
+			}
+			System.out.println();
+		}
+	}
+}
